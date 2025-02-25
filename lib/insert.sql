a942ebd057f96746925396b7991759c13e8f162b
Engineer-Collo <collinslikhomba@gmail.com>
2025-02-25 17:05:57 +0300

    initial commit

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (id,name,age,sex,color,temperament,alive) VALUES
+(1 "Mr Chocolate", 30, "M", "Brown" ,"Confident", "1"),
+(2 "Rowdy", 34, "M", "Black" ,"Creative", "1"), 
+(3 "Tabitha", 50, "F", "Brown" ,"Confident", "1"), 
+(4 "Segment Brown", 60, "M", "Brown" ,"Confident", "0"), 
+(5 "Melisa", 70, "F", "Black" ,"artistic", "0"), 
+(6 "Grinch", 3,8 "M", "Brown" ,"Confident", "1"), 
+(7 "Wendy",46, "F", "Black" ,"mood", "0"), 
+ (8 "NULL", 27, "M", "Brown" ,"Confident", "1"); 
